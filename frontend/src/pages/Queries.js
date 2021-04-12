aea889fc033b01621efbb65772d145828decfb8d
Renzo <renzol2@illinois.edu>
2021-04-12 15:24:25 -0500

    Finish displaying query results

--- frontend/src/pages/Queries.js
+++ frontend/src/pages/Queries.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import '../Queries.css';
 import Axios from 'axios';
 
 export default function RunQueries() {
+  let key = 0;
   const searchFormRef = useRef(null);
-  const ADVANCED_QUERY_ENDPOINT = 'http://localhost:3002/api/advanced/';
+  const ADVANCED_QUERY_ENDPOINT = 'http://localhost:3002/api/advanced';
   const LIMIT = 50;
   const [responseList1, setResponseList1] = useState([]);
   const [responseList2, setResponseList2] = useState([]);
   const [responseList3, setResponseList3] = useState([]);
   const [responseList4, setResponseList4] = useState([]);
 
-  const [query_number, setQueryNumber] = useState('');
+  const [query_number, setQueryNumber] = useState(1);
 
   function runQuery() {
-    Axios.get(
-      `${ADVANCED_QUERY_ENDPOINT}/${query_number}/${LIMIT}`
-    ).then((response) => {
+    const URL = `${ADVANCED_QUERY_ENDPOINT}/${query_number}/${LIMIT}`;
+    Axios.get(URL).then((response) => {
       if (query_number === 1) {
         setResponseList1(response.data);
       } else if (query_number === 2) {
@@ -36,7 +36,6 @@ export default function RunQueries() {
         textAlign: 'center',
       }}
     >
-
       <h3>Run an Advanced Query</h3>
       <form
         ref={searchFormRef}
@@ -46,16 +45,17 @@ export default function RunQueries() {
           searchFormRef.current.reset();
         }}
       >
-
-        <div class="row">
-          <div class="column">
+        <div className="row">
+          <div className="column">
             <label style={{ padding: 10 }}>
-              Returns name of song, user first and last name, and user date of birth of users that had a birth date on the same day as the release date of a song.
+              Returns name of song, user first and last name, and user date of
+              birth of users that had a birth date on the same day as the
+              release date of a song.
             </label>
             <br />
-            <input type="submit" onSubmit={() => setQueryNumber(1)} />
+            <input type="submit" onClick={() => setQueryNumber(1)} />
 
-             {/* Response list container */}
+            {/* Response 1 list container */}
             <div
               style={{
                 width: '100%',
@@ -65,8 +65,10 @@ export default function RunQueries() {
               }}
             >
               {responseList1.map((response) => (
-                <div key={response.name}>
-                  <h5>{response.first_name} {response.last_name}</h5>
+                <div key={key++}>
+                  <h5>
+                    {response.first_name} {response.last_name}
+                  </h5>
                   <p>{response.name}</p>
                   <p>{response.dob}</p>
                 </div>
@@ -74,30 +76,85 @@ export default function RunQueries() {
             </div>
           </div>
 
-          <div class="column">
+          <div className="column">
             <label style={{ padding: 10 }}>
               Returns tracks from the 21st century.
             </label>
             <br />
-            <input type="submit" onSubmit={() => setQueryNumber(2)} />
+            <input type="submit" onClick={() => setQueryNumber(2)} />
+
+            {/* Response 2 list container */}
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {responseList2.map((response) => (
+                <div key={key++}>
+                  <h5>{response.track}</h5>
+                  <p>{response.year}</p>
+                </div>
+              ))}
+            </div>
           </div>
 
-          <div class="column">
+          <div className="column">
             <label style={{ padding: 10 }}>
-              Returns the top 50% of artists by their popularity (value ranging [0,100]).
+              Returns the top 50% of artists by their popularity (value ranging
+              [0,100]).
             </label>
             <br />
-            <input type="submit" onSubmit={() => setQueryNumber(3)} />
+            <input type="submit" onClick={() => setQueryNumber(3)} />
+
+            {/* Response 3 list container */}
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {responseList3.map((response) => (
+                <div key={key++}>
+                  <h5>{response.artName}</h5>
+                  <p>{response.artPop}</p>
+                </div>
+              ))}
+            </div>
           </div>
 
-          <div class="column">  
+          <div className="column">
             <label style={{ padding: 10 }}>
-              Returns the names of users and artists with popularity in the lower half and with the same first name.
+              Returns the names of users and artists with popularity in the
+              lower half and with the same first name.
             </label>
             <br />
-            <input type="submit" onSubmit={() => setQueryNumber(4)} />
-          </div>
+            <input type="submit" onClick={() => setQueryNumber(4)} />
 
+            {/* Response 4 list container */}
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {responseList4.map((response) => (
+                <div key={key++}>
+                  <h5>First name: {response.first_name}</h5>
+                  <p>Artist name: {response.name}</p>
+                  <p>
+                    User name: {response.first_name} {response.last_name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </form>
     </div>
