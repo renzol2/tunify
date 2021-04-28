65e520ad78c0e10bf232d56e987ccc4de205ea5c
Renzo <renzol2@illinois.edu>
2021-04-28 06:32:02 -0500

    Show all admin tables through dropdown

--- frontend/src/pages/admin/Admin.js
+++ frontend/src/pages/admin/Admin.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Center,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import Artists from './Artists';
+import Songs from './Songs';
 import Users from './Users';
+import Genres from './Genres';
+import Queries from './Queries';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 export default function Admin() {
+  const TABLE_ROUTES = [
+    { route: '/artists', name: 'Artists', page: <Artists /> },
+    { route: '/songs', name: 'Songs', page: <Songs /> },
+    { route: '/users', name: 'Users', page: <Users /> },
+    { route: '/genres', name: 'Genres', page: <Genres /> },
+    { route: '/queries', name: 'Queries', page: <Queries /> },
+  ];
+  const [currentTable, setCurrentTable] = useState(TABLE_ROUTES[0]);
+
   return (
     <Box>
-      <Users />
+      <Center>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            Select table
+          </MenuButton>
+          <MenuList>
+            {TABLE_ROUTES.map(({ route, name, page }, i) => (
+              <MenuItem
+                key={route}
+                onClick={() => setCurrentTable(TABLE_ROUTES[i])}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </Center>
+
+      {currentTable.page}
     </Box>
-  )
+  );
 }
