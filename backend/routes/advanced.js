3a6173631d65fe9ddde0293952c6d4f28c64168e
Renzo <renzol2@illinois.edu>
2021-04-28 06:42:36 -0500

    Put routes into separate files

--- backend/routes/advanced.js
+++ backend/routes/advanced.js
@@ -1,16 +1,5 @@
 const routes = require('express').Router();
-const mysql = require('mysql');
-
-var db = mysql.createConnection({
-  host: '35.223.243.139',
-  user: 'root',
-  password: '6hqIAjcwnhDdsC3x',
-  database: 'tunify',
-});
-
-db.connect(function (err) {
-  if (err) throw err;
-});
+const db = require('../db');
 
 /**
  * Queries MySQL database
