00be603b049b2b0df9545eac139333488294e007
Renzo <renzol2@illinois.edu>
2021-04-28 18:40:27 -0500

    Create search bar on search page

--- frontend/src/pages/SearchMusic.js
+++ frontend/src/pages/SearchMusic.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 
 export default function SearchMusic() {
   return (
-    <Box>
-      SearchMusic
+    <Box w="100%" p="5%">
+      <Heading as="h3" fontWeight="extrabold" fontSize="4xl" my={3}>
+        Search for your favorite songs, genres, and artists.
+      </Heading>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
+        <Input variant="filled" placeholder="Search"  />
+      </InputGroup>
     </Box>
-  )
+  );
 }
